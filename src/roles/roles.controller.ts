f0a863f94f2749722680c69ea5d112d6dad11a09
kirbi96 <repetitorkazan@mail.ru>
2023-01-07 01:05:21 +0300

    added registration

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { RolesService } from "./roles.service";
-import { ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Role } from "./roles.model";
 import { CreateRoleDto } from "./dto/create-role.dto";
 
+
+@ApiTags('Роли')
 @Controller('roles')
 export class RolesController {
   constructor(private roleService: RolesService) {}
